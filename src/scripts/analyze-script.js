057f9a7ac31aa8fab2a32d7b4631f7e321005680
Emily Domingos <emily.domingos@e-unicred.com.br>
2025-04-11 10:57:22 -0300

    Ajuste na análise de código

--- src/scripts/analyze-script.js
+++ src/scripts/analyze-script.js
@@ -12,7 +12,7 @@ async function analyze() {
     setTimeout(() => {
 
       exec(
-        `lighthouse http://localhost:8080/temp.html --output=json --quiet --chrome-flags="--headless --no-sandbox"`,
+        `npx lighthouse http://localhost:8080/temp.html --output=json --quiet --chrome-flags="--headless --no-sandbox"`,
         (err, stdout, stderr) => {
           if (err || stderr) {
             console.error('Error running Lighthouse:', err || stderr);
